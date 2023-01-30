0811d6a166c3b6fc6ee1449c5992cf86231cd4a8
w3irddy <118191333+w3irddy@users.noreply.github.com>
2023-01-30 18:42:35 +0300

    Delete script together (test).lua

--- script together (test).lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/w3irddy/projects/main/HWIDBlacklist.lua"))();
-loadstring(game:HttpGet("https://raw.githubusercontent.com/w3irddy/projects/main/HWIDWhitelist.lua"))();
