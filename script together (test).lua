f774fbda2f2f64bf24fb3f1192e70193658300e5
w3irddy <118191333+w3irddy@users.noreply.github.com>
2023-01-30 18:40:47 +0300

    Create script together (test).lua

---
+++ script together (test).lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/w3irddy/projects/main/HWIDBlacklist.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/w3irddy/projects/main/HWIDWhitelist.lua"))();
