eeef76f3dda531de7fb98dd579fc8005e061c56b
w3irddy <118191333+w3irddy@users.noreply.github.com>
2023-02-11 11:49:27 +0300

    testing reverted

--- HWIDWhitelist.lua
+++ HWIDWhitelist.lua
@@ -1,4 +1,3 @@
---[[
 return {
     "FA0D62E1-305E-44D4-B0E1-1B26B1059FEF", 
     "c04f4230-6a49-4680-ac9d-2e84c103223c",
@@ -6,5 +5,3 @@ return {
     "3F36FE49-9C48-4147-AD95-574AB93C11C0",
     "FDC1B954-5117-4B29-AFA3-86B05D98FD20"
 }
-]]
-return {"random"};
